bab46cbd5872dcc0cbedf418054b1b9b47ead906
antonovmike <antonovma@outlook.com>
2025-01-18 20:13:19 +0100

    [IMP] Fields Date of Birth, Age, Passport details, Contact person added to patient

--- models/patient.py
+++ models/patient.py
@@ -1,6 +1,8 @@
 import logging
+from datetime import date
+from dateutil.relativedelta import relativedelta
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 _logger = logging.getLogger(__name__)
 
@@ -10,10 +12,26 @@ class Patient(models.Model):
     _inherit = 'hr_hospital.person'
     _description = 'Patient'
 
-    physician_ids = fields.Many2many(
+    date_of_birth = fields.Date()
+    age = fields.Integer(compute='_compute_age', store=True)
+    passport_details = fields.Char()
+    contact_person = fields.Char()
+
+    personal_physician = fields.Many2many(
         comodel_name='hr.hospital.physician',
     )
 
     disease_ids = fields.Many2many(
         comodel_name='hr.hospital.disease',
     )
+
+    @api.depends('date_of_birth')
+    def _compute_age(self):
+        for rec in self:
+            if rec.date_of_birth:
+                rec.age = relativedelta(
+                    date.today(),
+                    date(rec.date_of_birth.year, rec.date_of_birth.month, rec.date_of_birth.day),
+                ).years
+            else:
+                rec.age = False
