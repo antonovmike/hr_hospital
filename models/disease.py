94f63ab287c003ce66d205bf812de63b0b3f1387
antonovmike <antonovma@outlook.com>
2025-01-18 21:38:50 +0100

    [IMP] added DiseaseCategory, changed Disease model - it is now a hierarchical structure

--- models/disease.py
+++ models/disease.py
@@ -5,9 +5,25 @@ from odoo import models, fields
 _logger = logging.getLogger(__name__)
 
 
+class DiseaseCategory(models.Model):
+    _name = 'hr.hospital.disease.category'
+    _description = 'Disease Category'
+
+    name = fields.Char(string='Category Name', required=True)
+    disease_ids = fields.One2many(
+        'hr.hospital.disease',
+        'category_id',
+        string='Diseases'
+        )
+
+
 class Disease(models.Model):
     _name = 'hr.hospital.disease'
     _description = 'Disease'
 
-    name = fields.Char(required=True)
-    active = fields.Boolean(default=True)
+    name = fields.Char(string='Disease Name', required=True)
+    category_id = fields.Many2one(
+        'hr.hospital.disease.category',
+        string='Disease Category',
+        required=True
+        )
