1582d1bbf83021db9b90db434a93efbd34d664ba
antonovmike <antonovma@outlook.com>
2025-01-19 07:41:50 +0100

    [IMP] for PatientVisits: Prohibit changing the time/date/physician of a visit that has already taken place

--- models/patient_visits.py
+++ models/patient_visits.py
@@ -1,6 +1,7 @@
 import logging
 
 from odoo import models, fields
+from odoo.exceptions import ValidationError
 
 _logger = logging.getLogger(__name__)
 
@@ -22,3 +23,16 @@ class PatientVisits(models.Model):
         comodel_name='hr.hospital.diagnosis',
         string='Diagnosis',
     )
+
+    def write(self, vals):
+        """Override write to prevent modification of past visits."""
+        for record in self:
+            if (record.start_date_and_time and 
+                    record.start_date_and_time < fields.Datetime.now()):
+                restricted_fields = {'start_date_and_time', 'physisian_id'}
+                if any(field in vals for field in restricted_fields):
+                    raise ValidationError(
+                        "Cannot modify the date/time or physician of a "
+                        "visit that has already taken place."
+                    )
+        return super().write(vals)
